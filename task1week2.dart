d12bd7dbfbf4fff7a367f1999cdcaecccf4024aa
Calvinrichie <148748872+Calvinrichie@users.noreply.github.com>
2024-09-30 09:52:45 +0800

    Add files via upload

---
+++ task1week2.dart
@@ -0,0 +1,25 @@
+class student {
+
+String name = 'michah';
+
+String studentid = '90087680001';
+
+int score = 89;
+
+void check() {
+
+  if  (score >=70) {
+  print ("passed"  ); }
+  else { print ("failed") ;}
+
+}
+
+}
+
+void main(){
+
+student check  = new student();
+
+check.check();
+
+}
